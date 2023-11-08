88e0701161e7f8fd0860d7536adf1e87ad1f9e6f
Avoler0 <jys1391@gmail.com>
2023-11-08 11:47:03 +0900

    init project

---
+++ index.js
@@ -0,0 +1,13 @@
+import { config } from 'dotenv';
+import express from 'express';
+
+config();
+
+const port = process.env.SERVER_PORT
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(port, '포트로 서버가 열렸어요!');
+});
